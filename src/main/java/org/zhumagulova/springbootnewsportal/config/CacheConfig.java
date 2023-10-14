cae4c0cca3ba7e0a9bb1c7380c288a1719120f00
aliya zhumagulova <zhumagul100@gmail.com>
2023-10-14 16:08:31 +0600

    remove kafka

--- src/main/java/org/zhumagulova/springbootnewsportal/config/CacheConfig.java
+++ src/main/java/org/zhumagulova/springbootnewsportal/config/CacheConfig.java
@@ -1,14 +1,13 @@
 package org.zhumagulova.springbootnewsportal.config;
 
-import org.apache.kafka.clients.admin.NewTopic;
+
 import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.kafka.annotation.KafkaListener;
-import org.springframework.kafka.config.TopicBuilder;
+
 
 import java.time.Duration;
 
